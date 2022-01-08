64d7b4d1ce4a5f73012970c157e1e33f9c7f101f
CHOI <jack96318@hanmail.net>
2022-01-09 01:46:50 +0900

    1/9 login page

---
+++ src/main/java/member/MemberConrtoller.java
@@ -0,0 +1,13 @@
+package member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberConrtoller {
+
+    @RequestMapping("/member/login")
+    public String login(){
+        return "/member/login";
+    }
+}
