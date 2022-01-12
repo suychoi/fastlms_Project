5ea0f9bc61d8428b7f2e31f91a7e739bf3bb7f73
CHOI <jack96318@hanmail.net>
2022-01-13 02:03:59 +0900

    1/13 login page

--- src/main/java/com/example/fastlms_project/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fastlms_project.member.repository;
-
-import com.example.fastlms_project.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
