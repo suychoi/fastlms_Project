0306310671a3865e2c583e312b1a55cdae8f18d5
CHOI <jack96318@hanmail.net>
2022-01-13 02:19:08 +0900

    1/13 login page

--- src/main/java/com/example/fastlms_project/member/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.example.fastlms_project.member.service;
-
-import com.example.fastlms_project.member.model.MemberRegister;
-
-public interface MemberService {
-    boolean register(MemberRegister parameter);
-}
