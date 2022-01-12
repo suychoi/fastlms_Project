67f8ada603b80215a1313515278818a2a8c52719
CHOI <jack96318@hanmail.net>
2022-01-13 02:04:11 +0900

    1/13 login page

---
+++ src/main/java/com/example/fastlms_project/member/service/MemberService.java
@@ -0,0 +1,7 @@
+package com.example.fastlms_project.member.service;
+
+import com.example.fastlms_project.member.model.MemberRegister;
+
+public interface MemberService {
+    boolean register(MemberRegister parameter);
+}
---
+++ src/test/java/com/example/fastlms_project/FastlmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastlms_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastlmsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
